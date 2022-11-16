7aa724725d02d7dc43bc97025de51dd2c1a116c4
JN <90158060+assejn@users.noreply.github.com>
2022-11-16 19:10:47 +0100

    Add files via upload

---
+++ TA-script.py
@@ -0,0 +1,37 @@
+import requests
+import json
+import csv
+
+headers = {'User-Agent': 'asse96 - Script for TA event'}
+offset = 0
+seasonID = ''
+mapID = ''
+
+
+header = ['Player', 'PlayerID', 'Time', 'Timestamp']
+with open('TA-data.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(header)
+
+    while True:
+        offsetString = str(offset)
+        url = 'https://trackmania.io/api/leaderboard/' + seasonID + '/' + mapID + '?offset=' + offsetString + '&length=100'
+        rawData = requests.get(url, headers=headers).json()
+        if 'tops' in rawData:
+            tops = rawData['tops']
+            if tops is not None and len(tops) > 0:
+                for top in tops:
+                    player = top['player']
+                    playerName = player['name']
+                    playerID = player['id']
+                    recordTime = top['time']
+                    recordTimestamp = top['timestamp']
+                    playerData = [playerName, playerID, recordTime, recordTimestamp]
+                    writer.writerow(playerData)
+
+            if tops is None or len(tops) < 100:
+                break
+            else:
+                offset = offset + 100
+        else:
+            break
